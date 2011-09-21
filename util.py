a80b19b83c13f87f85d6e634e3c4c62ac00b36c7
Christian Geier <geier@lostpackets.de>
2011-09-21 11:28:17 +0200

    adding the complete vcard to the database now, but there is no way to query it yet

---
+++ util.py
@@ -0,0 +1,13 @@
+
+None
+#!/usr/bin/env python
+# vim: set ts=4 sw=4 expandtab sts=4:
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <geier@lostpackets.de> wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return Christian Geier
+# ----------------------------------------------------------------------------
+
+def smartEncode(string):
+	return unicode(string).encode('utf-8', 'strict')
