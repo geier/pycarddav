bcaf5b1290bb59b6030bf1ec66a28b138894c215
Christian Geier <geier@lostpackets.de>
2012-11-13 10:52:44 +0100

    changed license to expat/mit
    fixes github issue #28

--- pycarddav/__init__.py
+++ pycarddav/__init__.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python
 # vim: set fileencoding=utf-8 :
+# Copyright (c) 2011-2012 Christian Geier, David Soulayrol
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import argparse
 import ConfigParser
@@ -13,7 +33,7 @@ __version__ = '0.4a'
 __author__ = 'Christian Geier'
 __author_email__ = 'pycarddav@lostpackets.de'
 __description__ = 'A CardDAV based address book tool'
-__license__ = 'BeerWare, see COPYING'
+__license__ = 'Expat/MIT, see COPYING'
 __homepage__ = 'http://pycarddav.lostpackets.de'
 
 
--- setup.py
+++ setup.py
@@ -5,35 +5,35 @@ import sys
 from setuptools import setup
 
 requirements = [
-        'lxml',
-        'vobject',
-        'requests',
-        'urwid',
-        ]
+    'lxml',
+    'vobject',
+    'requests',
+    'urwid'
+]
 if sys.version_info[:2] in ((2, 6),):
     # there is no argparse in python2.6
     requirements.append('argparse')
 
 setup(
-      name='pyCardDAV',
-      version = '0.4a',
-      description = 'A CardDAV based address book tool',
-      long_description=open('README.rst').read(),
-      author = 'Christian Geier',
-      author_email = 'pycarddav@lostpackets.de',
-      url = 'http://lostpackets.de/pycarddav',
-      license = 'Beerware',
-      packages = ['pycarddav'],
-      scripts=['bin/pycardsyncer', 'bin/pc_query', 'bin/pycard-import'],
-      requires=requirements,
-      classifiers=[
-          "Development Status :: 3 - Alpha",
-          "License :: Beerware",
-          "Environment :: Console :: Curses",
-          "Intended Audience :: End Users/Desktop",
-          "Operating System :: POSIX",
-          "Programming Language :: Python :: 2 :: Only",
-          "Topic :: Utilities",
-          "Topic :: Communications :: Email :: Address Book"
-          ],
-      )
+    name='pyCardDAV',
+    version='0.4a',
+    description='A CardDAV based address book tool',
+    long_description=open('README.rst').read(),
+    author='Christian Geier',
+    author_email='pycarddav@lostpackets.de',
+    url='http://lostpackets.de/pycarddav',
+    license='Expat/MIT',
+    packages=['pycarddav'],
+    scripts=['bin/pycardsyncer', 'bin/pc_query', 'bin/pycard-import'],
+    requires=requirements,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Environment :: Console :: Curses",
+        "Intended Audience :: End Users/Desktop",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 2 :: Only",
+        "Topic :: Utilities",
+        "Topic :: Communications :: Email :: Address Book"
+    ],
+)
