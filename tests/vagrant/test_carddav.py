ecf862d19f5315bb8ecec1dab2e683fadb3547b6
Christian Geier <geier@lostpackets.de>
2013-01-08 02:26:42 +0100

    changed vagrant test

--- tests/vagrant/test_carddav.py
+++ tests/vagrant/test_carddav.py
@@ -10,7 +10,7 @@ LENNA_BASE = 'http://localhost:8080/davical/caldav.php/lenna/addresses/'
 def test_url_does_not_exist():
     vbox = vagrant.Vagrant()
     vbox.up()
-    with pytest.raises(Exception):
+    with pytest.raises(carddav.requests.exceptions.HTTPError):
         carddav.PyCardDAV('http://localhost:8080/doesnotexist/')
 
 
